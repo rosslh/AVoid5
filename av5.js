ce27e0295bc8c300161a9039d6d52845487cf200
Ross Hill <rosshill@protonmail.com>
2016-11-15 23:56:54 -0500

    Create av5.js

---
+++ av5.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        av5
+// @namespace   http://rosshill.ca/
+// @description ...
+// @author      Ross Hill
+// @include     https://www.reddit.com/*
+// @include     https://np.reddit.com/*
+// @include     https://m.reddit.com/*
+// @version     1
+// @grant       none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+//
+// ==/UserScript==
+
+var textarea = $('.md textarea');
+var content = textarea.val();
+textarea.on('input',function(e){
+   content = textarea.val();
+   if(content != null){
+     if (content.includes('e') || content.includes('E')){
+       $('.save').css('display','none');
+     }
+     else{
+       $('.save').css('display','inline-block');
+     }
+   }
+})();
